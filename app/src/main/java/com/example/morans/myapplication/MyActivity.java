dd02dbe0ec0058cdd16a0943c886f99c1b9fdd8f
Sean Moran <sean.moran@agilex.com>
2014-08-14 15:41:27 -0400

    inital commit

---
+++ app/src/main/java/com/example/morans/myapplication/MyActivity.java
@@ -0,0 +1,73 @@
+package com.example.morans.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MyActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    // this method adds the action bar items to the menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+        //getMenuInflater().inflate(R.menu.my, menu);
+        //return true;
+    }
+    // This method acts as a listener for the menu bar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public void openSearch() {
+        Toast toast = Toast.makeText(this, "Pushed the search button", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void openSettings() {
+        Toast toast = Toast.makeText(this, "Pushed the settings button", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void sendMessage(View view) {
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}
